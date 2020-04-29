8d0daafefed97c96366c71d9890438aec37675ef
Alfredlau <1009273187@qq.com>
2020-04-30 00:22:52 +0800

    4/30 practise

---
+++ 145 Binary Tree Postorder Traversal.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+        
+class Solution(object):
+    def __init__(self):
+        self.l = []
+    def preorderTraversal(self, root):
+        if root == None:
+            return []
+        self.preorderTraversal(root.left)
+        self.preorderTraversal(root.right)
+        self.l.append(root.val)
+        return self.l
+    
+
+if __name__ == '__main__':
+    r1 = TreeNode(1)
+    r2 = TreeNode(7)
+    r3 = TreeNode(3)
+    r4 = TreeNode(4)
+    r5 = TreeNode(5)
+    
+    r1.left = r3
+    r1.right = r2
+    r3.left = r4
+    r4.left = r5
+    
+    s = Solution()
+    print(s.preorderTraversal(r1))
